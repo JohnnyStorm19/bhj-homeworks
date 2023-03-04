8fe88d2a5dc1120c804f38628cd4c1f7627f6baf
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-03-04 15:43:05 +0300

    Logic added to clicker

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -3,31 +3,42 @@ const clickerSpeedEl = document.getElementById('clicker__speed');
 const imageEl = document.getElementById('cookie');
 const clickerBlockEl = document.getElementById('clicker__status');
 
-let count = 0;
-let avgClick = 0;
-let countPerSecond = 0;
+let prevTimestamp = Date.now();
+function handleClick() {
+    counterEl.textContent++;
+    imageEl.width = +counterEl.textContent % 2 ? 220 : 200;
+    const currentTimestamp = Date.now();
+    const elapsedTime = currentTimestamp - prevTimestamp;
+    clickerSpeedEl.textContent = (1 / (elapsedTime / 1000)).toFixed(2);
+    prevTimestamp = currentTimestamp;
+}
+imageEl.onclick = handleClick;
 
-function getClicked() {
-    count++;
-    countPerSecond++;
-    counterEl.textContent = count;
-    imageEl.width === 200 ? toBiggerSize() : toNormalSize();
-    clickerSpeedEl.textContent = avgClick;
-}
+// let count = 0;
+// let avgClick = 0;
+// let countPerSecond = 0;
 
-setInterval(incrementInterval, 1000);
-//Функции для работы с картинкой
-function toBiggerSize() {
-    imageEl.width = 220; 
-    imageEl.height = 140;
-}
-function toNormalSize() {
-    imageEl.width = 200;
-    imageEl.height = 128;
-}
+// function getClicked() {
+//     count++;
+//     countPerSecond++;
+//     counterEl.textContent = count;
+//     imageEl.width === 200 ? toBiggerSize() : toNormalSize();
+//     clickerSpeedEl.textContent = avgClick;
+// }
+
+// setInterval(incrementInterval, 1000);
+// Функции для работы с картинкой
+// function toBiggerSize() {
+//     imageEl.width = 220;
+//     imageEl.height = 140;
+// }
+// function toNormalSize() {
+//     imageEl.width = 200;
+//     imageEl.height = 128;
+// }
 
 //Функция-колбек для подсчета кликов каждую секунду
-function incrementInterval() {
-    avgClick = countPerSecond;
-    countPerSecond = 0;
-}
+// function incrementInterval() {
+//     avgClick = countPerSecond;
+//     countPerSecond = 0;
+// }
