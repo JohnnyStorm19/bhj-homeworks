3e61067dbd856001d13e1188ae820d8537edc9f1
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-03-22 17:18:27 +0300

    initial commit

---
+++ document-structure/cart/task.js
@@ -0,0 +1,96 @@
+const products = document.querySelector('.products');
+const cart = document.querySelector('.cart');
+const cartProducts = cart.querySelector('.cart__products');
+let dataId = JSON.parse(localStorage.getItem('dataId')) || []; //массив с ID добавленных в корзину товаров
+let productInfo = JSON.parse(localStorage.getItem('productInfo')) || []; //массив с объектами (id карточки, ссылка на img, кол-во единиц)
+
+products.addEventListener('click', (event) => {
+    let target = event.target;
+    //увеличиваем-уменьшаем количество товара
+    if (target.classList.contains('product__quantity-control_inc')) {
+        let value = target.previousElementSibling;
+        value.textContent++;
+    }
+    if (target.classList.contains('product__quantity-control_dec')) {
+        let value = target.nextElementSibling;
+        if (value.textContent < 2) return;
+        value.textContent--;
+    }
+    //добавляем товар в корзину
+    if (target.classList.contains('product__add')) {
+        const product = target.closest('.product');
+        const productCounter = product.querySelector('.product__quantity-value');
+
+        if (!dataId.includes(product.dataset.id)) {
+            let cartProduct = {
+                id: product.dataset.id,
+                count: Number(product.querySelector('.product__quantity-value').textContent),
+                img: product.querySelector('.product__image').getAttribute('src')
+            }
+            dataId.push(product.dataset.id); //пушим ID добавленного товара в массив с ID
+            productInfo.push(cartProduct); // пушим объект с инфо о добавленном товаре в массив
+            addProductToCart(productInfo, cartProducts); //добавляем разметку карточек товаров в корзину
+            displayCart(); //показываем корзину
+            localStorage.setItem('productInfo', JSON.stringify(productInfo));
+            localStorage.setItem('dataId', JSON.stringify(dataId));
+            console.log('Сработала вставка через inner');
+        } else {
+            const productsInCart = [...cartProducts.querySelectorAll('.cart__product')];
+            productsInCart.forEach(el => {
+                if (el.dataset.id === product.dataset.id) {
+                    let cartValue = el.querySelector('.cart__product-count');
+                    cartValue.textContent = +cartValue.textContent + +productCounter.textContent;
+                    let objToChange = productInfo.find(item => item.id === el.dataset.id);
+                    productInfo[productInfo.indexOf(objToChange)].count += +productCounter.textContent;
+                    localStorage.setItem('productInfo', JSON.stringify(productInfo));
+                    console.log('Объект уже вставлен');
+                }
+            })
+        }
+    }
+})
+
+cartProducts.addEventListener('click', (event) => { //обработчик для корзины товаров
+    let target = event.target;
+    if (target.classList.contains('cart-remove')) {
+        event.preventDefault();
+        const currentProduct = target.closest('.cart__product'); 
+        const currentId = currentProduct.dataset.id;
+        currentProduct.remove();
+        dataId.splice(dataId.indexOf(currentId), 1); //удаляю ID из массива с ID
+        let indexOfProductInfo = productInfo.indexOf(productInfo.find(el => el.id === currentId)); //получаю индекс элемента, который нужно удалить в массиве с объектами
+        productInfo.splice(indexOfProductInfo, 1);
+        localStorage.setItem('productInfo', JSON.stringify(productInfo));
+        localStorage.setItem('dataId', JSON.stringify(dataId));
+        displayCart();
+    }
+})
+
+//функция для отображения корзины
+function displayCart() {
+    !cartProducts.children.length ? cart.style.display = 'none' : cart.style.display = 'block';
+}
+
+//функция для вставки элементов корзины
+function addProductToCart(objArr, cartDiv) {
+    cartDiv.innerHTML = objArr.map(el => {
+        return `<div class="cart__product" data-id=${el.id}>
+        <img src=${el.img} alt="" class="cart__product-image">
+        <div class="cart__product-count">${el.count}</div>
+        <a href="#" class="cart-remove">x</a>
+    </div>
+        `
+    }).join('');
+}
+
+addProductToCart(productInfo, cartProducts);
+displayCart();
+
+
+
+
+
+
+
+
+
