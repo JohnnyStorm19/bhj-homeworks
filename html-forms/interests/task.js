47ea92ebc6a04bdf60b87677e1de724aec2025a4
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-03-22 16:50:59 +0300

    improved logic in task.js

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,51 +1,39 @@
 const interestsMain = document.querySelector('.interests_main');
-const interests = Array.from(interestsMain.querySelector('ul').children);
 
-interests.forEach(list => list.addEventListener('change', (event) => {
-  if (event.target === list.querySelector('label input')) {
-    activeSubChecks(event.target);
-  } else {
-    indeterminateCount(event.target);
-  }
-}))
-
-function activeSubChecks(element) {
-  const subCheck = Array.from(element.closest('label').nextElementSibling.querySelectorAll('input')); //нужно иначе находить сабчек
-  if(subCheck) {
-    if (element.checked) {
-      subCheck.forEach(el => el.checked = true);
-    } else {
-      subCheck.forEach(el => el.checked = false);
-    }
-    while(subCheck) { 
-      activeSubChecks(element);
-      indeterminateCount(element);
+interestsMain.addEventListener('change', (event) => { //вешаем обработчик на главный блок
+  let target = event.target;
+  if (target.classList.contains('interest__check')) { //если нажали на чекбокс
+    if (target.closest('label').nextElementSibling) { //если нажали на "родительский" чекбокс
+      childrenCheck(target);
+    } else if (target.closest('.interests_active')) { //если нажали на "дочерний" чекбокс
+      parentCheck(target);
     }
   }
+})
+
+//функция для установки состояния "дочерних" чекбосков по нажатию на "родительский" чекбокс
+function childrenCheck(element) {
+  const inputs = [...element.closest('label').nextElementSibling.querySelectorAll('input')];
+  element.checked ? inputs.forEach(el => el.checked = true) : inputs.forEach(el => el.checked = false);
+  parentCheck(element); //проверяем "родительские" чекбоксы на наличие у них "родителей"
 }
+//функция для установки состояния "родительских" чекбоксов по нажатию на "дочерний" чекбокс
+function parentCheck(element) {
+  if (!element.closest('.interests_active')) { //базовый случай, когда завершим функцию: если выбрали самый крайний элемент ("родителя" всех и вся)
+    return;
+  }
+  const subChecksBlock = element.closest('.interests_active'); //находим блок для "дочерних" элементов (ul)
+  const inputs = [...subChecksBlock.querySelectorAll('input')]; //у найденного блока находим все чекбоксы
+  const parent = subChecksBlock.closest('.interest').querySelector('input'); //находим "родителя" для "дочернего" чекбокса
 
-function indeterminateCount(element) {
-  const subChecksBlock = element.closest('.interests_active');
-  const mainCheck = subChecksBlock.previousElementSibling.querySelector('input');
-  const inputs = Array.from(subChecksBlock.querySelectorAll('input'));
-  let countTrue = 0;
-  let countFalse = 0;
-  for(let check of inputs) {
-    if(check.checked) {
-      countTrue++;
-    } else {
-      countFalse++;
-    }
+  if (inputs.every(el => el.checked)) { 
+    parent.checked = true;
+    parent.indeterminate = false;
+  } else if (inputs.every(el => el.checked === false)) {
+    parent.checked = false;
+    parent.indeterminate = false;
+  } else { 
+    parent.indeterminate = true; 
   }
-  
-  if (countTrue === 0 && countFalse > 0) {
-    mainCheck.indeterminate = false;
-    mainCheck.checked = false;
-  } else if(countTrue > 0 && countFalse === 0){
-    mainCheck.indeterminate = false;
-    mainCheck.checked = true;
-  } else if(countFalse > 0 && countTrue > 0){
-    mainCheck.indeterminate = true;
-  }
-  console.log(mainCheck, countTrue, countFalse)
+  parentCheck(parent); //рекурсивно проходимся по "родителям" на наличие у них "родителей". Если достигаем самого крайнего - базовый случай
 }
