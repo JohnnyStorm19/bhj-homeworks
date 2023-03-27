390662a75e19e09fb76fd109f1db440f9e473f40
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-03-27 13:53:16 +0300

    Update task.js

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,38 +1,58 @@
-const tooltips = Array.from(document.querySelectorAll('.has-tooltip'));
-const newTooltip = document.createElement('span');
-newTooltip.classList.add('tooltip');
+const hasTooltips = Array.from(document.querySelectorAll('.has-tooltip'));
+hasTooltips.forEach(el => el.insertAdjacentHTML('afterend', '<div class="tooltip"></div>'));
+
 
 document.addEventListener('click', (event) => {
-    if (event.target.classList.contains('has-tooltip')) {
-        activeTooltip(event);
+    let target = event.target;
+    if (target.classList.contains('has-tooltip')) {
+        event.preventDefault();
+        const tooltip = target.nextElementSibling;
+        tooltipContent(target, tooltip);
+        changePositionAttribute(tooltip, 'bottom');
+        if (tooltip.classList.contains('tooltip_active')) {
+            tooltip.classList.remove('tooltip_active');
+        } else {
+            hideTooltips();
+            tooltip.classList.add('tooltip_active');
+            tooltipPositioning(tooltip, tooltip.dataset.position, target);
+        }
     } else {
-        newTooltip.classList.remove('tooltip_active');
+        hideTooltips();
     }
 })
 
-function activeTooltip(event) {
-    event.preventDefault();
-    event.target.style.position = 'relative';
-    newTooltip.style.position = 'absolute'
-    event.target.appendChild(newTooltip);
-    newTooltip.setAttribute('data-position', 'bottom');
-    newTooltip.textContent = event.target.getAttribute('title');
-
-    if (newTooltip.dataset.position === 'top') {
-        newTooltip.style.bottom = '100%';
-        newTooltip.style.left = 0 + 'px';
-    } else if (newTooltip.dataset.position === 'left') {
-        newTooltip.style.right = '100%';
-        newTooltip.style.top = 0 + 'px';
-
-    } else if (newTooltip.dataset.position === 'right') {
-        newTooltip.style.left = '100%';
-        newTooltip.style.top = 0 + 'px';
-
-    } else if (newTooltip.dataset.position === 'bottom') {
-        newTooltip.style.top = '100%';
-        newTooltip.style.left = 0 + 'px';
+//смена позиций для подсказки
+function tooltipPositioning(element, position, event) {
+    let { top: linkTop, left: linkLeft, height: linkHeight, width: linkWidth} = event.getBoundingClientRect();
+    let { height: tooltipHeight, width: tooltipWidth } = element.getBoundingClientRect();
+    element.style.position = 'absolute';
+    if (position === 'top') {
+        element.style.top = linkTop + window.scrollY - tooltipHeight + 'px';
+        element.style.left = linkLeft + 'px';
+    } else if (position === 'left') {
+        element.style.top = linkTop + window.scrollY + 'px';
+        element.style.left = linkLeft - tooltipWidth + 'px';
+    } else if (position === 'right') {
+        element.style.top = linkTop + window.scrollY + 'px';
+        element.style.left = linkLeft + linkWidth + 'px';
+    } else if (position === 'bottom') {
+        element.style.top = linkTop + window.scrollY + linkHeight + 'px';
+        element.style.left = linkLeft + 'px';
     }
-
-    newTooltip.classList.add('tooltip_active');
+}
+//скрываем подсказки
+function hideTooltips() {
+    const currentTips = document.querySelectorAll('.tooltip_active');
+    currentTips.forEach(el => {
+        el.classList.remove('tooltip_active');
+    })
+}
+//устанавливаем атрибут и наполняем подсказку текстовым контентом
+function tooltipContent(event, element) {
+    element.setAttribute('data-position', '');
+    element.textContent = event.getAttribute('title');
+}
+//смена позиции
+function changePositionAttribute(element, position) {
+    element.dataset.position = position
 }
